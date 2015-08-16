de560673ec7a9abceb860a60bfdee51c731cb22e
song <song@songdeMacBook-Pro.local>
2015-08-17 07:27:46 +0800

    Coding complete

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -1,3 +1,9 @@
+/***************************************************************************
+ *
+ * $HW3: 8/8/2015 thinkhy  Implement malloc
+ *
+ ***************************************************************************/
+
 /*
  * mm_alloc.c
  *
@@ -8,25 +14,156 @@
 #include "mm_alloc.h"
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
 
 /* Your final implementation should comment out this macro. */
-#define MM_USE_STUBS
+/* #define MM_USE_STUBS */
+#define ASSERT     assert
+#define align4(x)  (((((x)-1)>>2)<<2)+4)
 
 void* mm_malloc(size_t size)
 {
 #ifdef MM_USE_STUBS
     return calloc(1, size);
 #else
-#error Not implemented.
+/* #error Not implemented.  @HW3D */
 #endif
 }
+
+/* Global variables                                                   */
+s_block_ptr base;     /* beginning point of heap                @HW3A */
+s_block_ptr last;     /* last visited block                     @HW3A */
+
+/* Internal functions                                           @HW3A */
+
+/* Split block according to size, b must exist */
+/* void split_block (s_block_ptr b, size_t s); */
+
+/* Try fusing block with neighbors             */
+/* s_block_ptr fusion(s_block_ptr b);          */
+
+/* Get the block from addr                     */
+/* s_block_ptr get_block (void *p);            */
+
+/* Add a new block at the of heap,
+ * return NULL if things go wrong
+ */
+/* s_block_ptr extend_heap (s_block_ptr last , size_t s); */
+
+
+/* Traverse the chunks list and stop when find a free block with enough space */
+static s_block_ptr find_block(size_t size)    /*                @HW3A */
+{  
+  s_block_ptr p = base;
+  for (; p != NULL; p->next)                  /*                @HW3A */
+   {
+      if (p->free && p->size >= size)         /*                @HW3A */
+          return p;                           /*                @HW3A */
+   }
+  
+  return NULL;                                /*                @HW3A */
+}
+
+/* Invoke sbrk to extend break point                            @HW3A */
+static s_block_ptr extend_heap (size_t size)                 /* @HW3A */
+{    
+    /* Current break is address of the new block                      */
+    s_block_ptr new_block = (s_block_ptr)sbrk(0);            /* @HW3A */
+    if ((int)sbrk(size + S_BLOCK_SIZE) == -1)                     /* @HW3A */ 
+        return NULL;                                         /* @HW3A */
+    new_block->size = size;                                  /* @HW3A */
+    new_block->ptr  = new_block;                             /* @HW3A */
+    new_block->free = TRUE;                                  /* @HW3A */
+     
+    if (base == NULL) /* heap is empty                          @HW3A */
+    {
+        new_block->prev = new_block->prev = NULL; /*            @HW3A */
+        base = new_block;                          /*           @HW3A */
+    }
+    else 
+    {
+        ASSERT(last->next == NULL);                /*           @HW3A */
+        new_block->prev = last;                  /*             @HW3A */
+        last->next = new_block;                  /*             @HW3A */
+    }
+     
+    return new_block;                            /*             @HW3A */
+}
+
+static s_block_ptr split_block(s_block_ptr p, size_t new_size)/*@HW3A */
+{
+    s_block_ptr new_block = NULL;              
+    if (p->size >= new_size + S_BLOCK_SIZE + 4)
+    {
+        /* Shorten the block'size to new_size */
+        p->size = new_size;  
+
+        /* Define a new block on the address p->data+size            */
+        new_block = (s_block_ptr)(p->data + new_size);
+        new_block->size = p->size - new_size - S_BLOCK_SIZE;
+        new_block->ptr = new_block;
+
+        /* Insert the new block after block p */
+        new_block->next = p->next;
+        new_block->prev = p;
+        if (p->next)
+            p->next->prev = new_block;
+        p->next = new_block;
+    }
+
+    return new_block;
+}
+
+static s_block_ptr fusion_block(s_block_ptr pb)
+{
+    ASSERT(pb->free == TRUE); 
+
+    /* If p's next block exists and p's block is also free, 
+     * then merge p and p's next block.                    */
+    if (pb->next && pb->next->free) 
+    {
+      pb->next = pb->next->next; 		
+      if (pb->next->next)
+        pb->next->next->prev = pb;
+      pb->size = pb->size + BLOCK_SIZE + pb->next->size;
+    }
+
+    return (pb);
+}
+
+static int is_valid_block_addr (void *p)
+{
+   if (p == NULL)
+	return FALSE;
+
+   s_block_ptr pb = (s_block_ptr)p;
+   if(align4((int)(pb + BLOCK_SIZE + pb->size)) > (int)sbrk(0))  
+	return FALSE;
+   if(pb->ptr != pb)
+	return FALSE;
+
+   return TRUE;
+}
+
+static s_block_ptr get_block(void *p)
+{
+    /* if p is a valid block address, convert it to a s_block_ptr */
+    if (is_valid_block_addr(p))
+	return (s_block_ptr)p;	
+    else
+        return NULL;
+}
+
 
 void* mm_realloc(void* ptr, size_t size)
 {
 #ifdef MM_USE_STUBS
     return realloc(ptr, size);
 #else
-#error Not implemented.
+    /* #error Not implemented. */
+    /* TODO */
+    do {}while(0); 
 #endif
 }
 
@@ -35,6 +172,32 @@ void mm_free(void* ptr)
 #ifdef MM_USE_STUBS
     free(ptr);
 #else
-#error Not implemented.
+    /* #error Not implemented. */
+    s_block_ptr pb;
+    /* If the pointer ptr is valid and get a non-NULL block address */
+    if ((pb=get_block(ptr)) != NULL)
+    {
+       pb->free = TRUE;  
+       /* Step backward and fusion two blocks */
+       if(pb->prev && pb->prev->free)
+         fusion_block(pb->prev);
+       /* Also try fusion with the next block */
+       if(pb->next)
+         fusion_block(pb);
+       /* Else we are the last pointer, free the end of heap   */
+       else
+       {
+          /* If we are also the head pointer, no more block    */
+          if (pb->prev == NULL)
+              base = NULL;
+          /* Else release the end of heap                      */
+          else
+              pb->prev->next = NULL;     
+          sbrk(pb);
+       }
+    }
+    
 #endif
 }
+
+
--- hw3/mm_alloc.h
+++ hw3/mm_alloc.h
@@ -2,6 +2,7 @@
  * mm_alloc.h
  *
  * Exports a clone of the interface documented in "man 3 malloc".
+ *
  */
 
 #pragma once
@@ -9,7 +10,7 @@
 #ifndef _malloc_H_
 #define _malloc_H_
 
- /* Define the block size since the sizeof will be wrong */
+/* Define the block size since the sizeof will be wrong */
 #define BLOCK_SIZE 40
 
 #ifdef __cplusplus
@@ -21,7 +22,6 @@ extern "C" {
 void* mm_malloc(size_t size);
 void* mm_realloc(void* ptr, size_t size);
 void mm_free(void* ptr);
-
 
 typedef struct s_block *s_block_ptr;
 
@@ -36,20 +36,9 @@ struct s_block {
     char data [0];
  };
 
-/* Split block according to size, b must exist */
-void split_block (s_block_ptr b, size_t s);
-
-/* Try fusing block with neighbors */
-s_block_ptr fusion(s_block_ptr b);
-
-/* Get the block from addr */
-s_block_ptr get_block (void *p);
-
-/* Add a new block at the of heap,
- * return NULL if things go wrong
- */
-s_block_ptr extend_heap (s_block_ptr last , size_t s);
-
+#define S_BLOCK_SIZE sizeof(struct s_block)
+#define TRUE         1
+#define FALSE        0
 
 #ifdef __cplusplus
 }
